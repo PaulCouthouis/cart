cdff007e66907914ff439176d2a158bb31b6e1e1
Paul Couthouis <couthouis.p@baseconnect.in>
2022-12-27 13:25:01 +0900

    core: only tax total

--- src/app/__tests__/add-article.spec.ts
+++ src/app/__tests__/add-article.spec.ts
@@ -1,18 +1,38 @@
 describe('Use Case: Add product in cart', () => {
   it('should add new article in cart', () => {
+    const { thenIncludingTaxTotalIs, thenOnlyTaxTotalIs } = setup();
+
+    thenOnlyTaxTotalIs(10);
     thenIncludingTaxTotalIs(100);
   });
 });
 
-const thenIncludingTaxTotalIs = (expectedTotal: number) => {
-  const cart = new Cart(100);
-  expect(cart.getIncludingTaxTotal()).toBe(expectedTotal);
+const setup = () => {
+  const cart = new Cart(100, 10);
+
+  const thenIncludingTaxTotalIs = (expectedTotal: number) => {
+    expect(cart.getIncludingTaxTotal()).toBe(expectedTotal);
+  };
+
+  const thenOnlyTaxTotalIs = (expectedTotal: number) => {
+    expect(cart.getOnlyTaxTotal()).toBe(expectedTotal);
+  };
+
+  return { thenIncludingTaxTotalIs, thenOnlyTaxTotalIs };
 };
 
 class Cart {
-  constructor(private includingTaxTotal: number) {}
+  constructor(
+    private readonly includingTaxTotal: number,
+    private readonly onlyTaxTotal: number
+  ) {}
 
   getIncludingTaxTotal() {
     return this.includingTaxTotal;
   }
+
+  getOnlyTaxTotal() {
+    return this.onlyTaxTotal;
+  }
 }
+
